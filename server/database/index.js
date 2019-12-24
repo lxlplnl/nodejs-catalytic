fe08a790e931228ed135f1824ef7ae2f1af4237e
Abdülkadir Karabaş <abdulkadir.karabas@akinon.com>
2019-12-25 00:23:03 +0300

    fix deprecated configs in mongoose connection

--- server/database/index.js
+++ server/database/index.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 import User from './models/user';
 
 const connectDb = () => {
-  return mongoose.connect(process.env.DATABASE_URL);
+  return mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
 };
 const models = { User };
 export { connectDb };
