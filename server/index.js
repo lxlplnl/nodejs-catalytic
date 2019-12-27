c4227582636a090111503a0d34d6fb492ed9fbe1
Abdülkadir Karabaş <abdulkadir.karabas@akinon.com>
2019-12-28 00:28:33 +0300

    added morgan logger

--- server/index.js
+++ server/index.js
@@ -5,18 +5,30 @@ import cors from 'cors';
 import jwtMV from 'express-jwt';
 import jwt from 'jsonwebtoken';
 import * as bodyParser from 'body-parser';
+import morgan from 'morgan';
+import fs from 'fs';
+import uuidMV from './middlewares/uuidMV';
 import routes from './routes';
 import DB from './database';
 
 const app = express();
 
 app.use(cors());
+app.use(uuidMV);
+app.use(
+  morgan(
+    ':id :date[iso] :status :method\t  :res[content-length]\t :response-time ms\t :url',
+    {
+      stream: fs.createWriteStream('access.log', {
+        flags: 'a',
+      }),
+    },
+  ),
+);
 
-app.use((req, res, next) => {
-  console.log(new Date(), req.method, req.url);
-  next();
+morgan.token('id', function getId(req) {
+  return req.id;
 });
-
 app.use(
   jwtMV({
     secret: process.env.JWT_SECRET,
